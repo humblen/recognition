7b69a651716fe737736b3a38ad0ecf4b20b00321
jiangmingyu <1538766430@qq.com>
2025-03-29 18:08:35 +0800

    modules create

--- runner/src/main/java/com/jay/recognition/runner/RecognitionApplication.java
+++ runner/src/main/java/com/jay/recognition/runner/RecognitionApplication.java
@@ -1,4 +1,4 @@
-package com.jay.recognition;
+package com.jay.recognition.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
